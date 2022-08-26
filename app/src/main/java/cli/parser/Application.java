aa85561890ec91ddf8256aa75fd5f8184b1e0edf
Sameer Kulkarni <cricketsamya@gmail.com>
2022-08-26 15:43:35 +0200

    Basic project added

---
+++ app/src/main/java/cli/parser/Application.java
@@ -0,0 +1,12 @@
+
+package cli.parser;
+
+public class Application {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Application().getGreeting());
+    }
+}
